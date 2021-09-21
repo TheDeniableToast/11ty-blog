34e5c426d7dcaa30ed775e6eb4a430dc281ab982
Filip Norberg <45309184+TheDeniableToast@users.noreply.github.com>
2021-09-21 14:47:15 +0200

    changed repo api link

--- src/_data/repos.js
+++ src/_data/repos.js
@@ -2,7 +2,7 @@ const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async function () {
     try {
-        let json = await Cache('https://api.github.com/users/filipnorberg', {
+        let json = await Cache('https://api.github.com/users/filipnorberg/repos', {
             duration: '1d', // 1 day
             type: 'json' // also supports "text" or "buffer"
         });
