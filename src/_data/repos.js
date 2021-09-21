f6716a41f016cf8192be6a6c010cc13599960bc4
Filip Norberg <45309184+TheDeniableToast@users.noreply.github.com>
2021-09-21 12:10:50 +0200

    🎨

--- src/_data/repos.js
+++ src/_data/repos.js
@@ -1,9 +1,15 @@
-const axios = require('axios');
+const Cache = require('@11ty/eleventy-cache-assets');
 
-module.exports = async () => {
-    const { data } = await axios.get('https://api.github.com/users/filipnorberg/repos');
-
-    console.log(data);
-
-    return data;
+module.exports = async function () {
+    try {
+        let json = await Cache('https://api.github.com/users/filipnorberg', {
+            duration: '1d', // 1 day
+            type: 'json' // also supports "text" or "buffer"
+        });
+        console.log(json);
+        return json;
+    } catch (e) {
+        console.log(e);
+        return {};
+    }
 };
