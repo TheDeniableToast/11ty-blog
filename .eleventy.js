6646dc359a460c38fbd4f3931b3a1264dd20635d
Filip Norberg <45309184+TheDeniableToast@users.noreply.github.com>
2021-09-21 12:11:20 +0200

    implemented rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 // .eleventy.js
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
 
